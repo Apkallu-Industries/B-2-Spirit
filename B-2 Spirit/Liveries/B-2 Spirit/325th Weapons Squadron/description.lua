0e570368fb44a4987ba0a4d5671ac7ae21e00e2c
Dany Mitchel <217238818+Apkallu-Industries@users.noreply.github.com>
2026-09-13 15:40:14 +0100

    feat(liveries): add 509th BW, 13th BS, 393d BS, and 325th WPS squadron liveries and painted decals

---
+++ B-2 Spirit/Liveries/B-2 Spirit/325th Weapons Squadron/description.lua
@@ -0,0 +1,18 @@
+livery = {
+    {"B2_Airframe_Mat", 0,  "B2_Airframe_Diffuse", true},
+    {"B2_Airframe_Mat", 1,  "B2_Airframe_Normal", true},
+    {"B2_Airframe_Mat", 13, "B2_Airframe_RoughMet", true},
+
+    {"B2_Mech_Mat",     0,  "B2_Mech_Diffuse", true},
+    {"B2_Mech_Mat",     1,  "B2_Mech_Normal", true},
+    {"B2_Mech_Mat",     13, "B2_Mech_RoughMet", true},
+
+    {"B2_Glass_Mat",    0,  "B2_Glass_Diffuse", true},
+    {"B2_Glass_Mat",    13, "B2_Glass_RoughMet", true},
+}
+
+name = "325th Weapons Squadron (USAF Weapons School)"
+countries = {
+    "USA",
+    "USAF Aggressors",
+}
