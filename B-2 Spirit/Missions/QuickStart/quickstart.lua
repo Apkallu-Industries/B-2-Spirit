8772362e9580acce8f4acefe71e515c362100c41
Dany Mitchel <217238818+Apkallu-Industries@users.noreply.github.com>
2026-09-13 15:19:59 +0100

    Add B-2 Spirit QuickStart and Single missions with high altitude and runway hot slots

--- B-2 Spirit/Missions/QuickStart/quickstart.lua
+++ B-2 Spirit/Missions/QuickStart/quickstart.lua
@@ -1,2 +1,16 @@
+-- B-2 Spirit Quickstart Missions for DCS World Instant Action
+
 planes = {
+    {
+        name = _('Free Flight (Choose FL350 Airborne or Runway Hot)'),
+        file = 'B-2_Spirit_Free_Flight.miz',
+    },
+    {
+        name = _('High Altitude Ingress (35,000 ft Airborne)'),
+        file = 'B-2_Spirit_High_Altitude.miz',
+    },
+    {
+        name = _('Runway Takeoff (Senaki-Kolkhi Hot)'),
+        file = 'B-2_Spirit_Runway_Hot.miz',
+    },
 }
